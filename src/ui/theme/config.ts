3924057564cff30ddab06cffe017f1f21d40de3a
davidcrc <david_drcr@hotmail.com>
2023-08-26 10:33:54 -0500

    style: :zap: config fonts globand and on-demand

---
+++ src/ui/theme/config.ts
@@ -0,0 +1,11 @@
+const FONT_FAMILY = {
+  inter: ["var(--font-inter)"],
+  mono: ["var(--font-roboto-mono)"],
+};
+
+const themeConfig = {
+  // colors: COLORS,
+  fontFamily: FONT_FAMILY,
+};
+
+export default themeConfig;
