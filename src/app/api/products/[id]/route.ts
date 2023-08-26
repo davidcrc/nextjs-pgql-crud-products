b98a0bc2b6fc2a0d11f0cdc20394ad11fe2d86bd
davidcrc <david_drcr@hotmail.com>
2023-08-26 10:51:22 -0500

    feat: delete product by uuid

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
+import { Prisma } from "@prisma/client";
 
 interface Params {
   params: { id: string };
@@ -30,8 +31,33 @@ export async function GET(_: NextRequest, { params }: Params) {
   }
 }
 
-export function DELETE() {
-  return NextResponse.json({ message: "deleting product" });
+export async function DELETE(_: NextRequest, { params }: Params) {
+  const { id } = params;
+
+  try {
+    const product = await prisma.product.delete({
+      where: {
+        uuid: id,
+      },
+    });
+
+    if (!product) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(product);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return NextResponse.json({ message: "Product found" }, { status: 404 });
+      }
+
+      return NextResponse.json(error.message, { status: 500 });
+    }
+  }
 }
 
 export function PUT() {
