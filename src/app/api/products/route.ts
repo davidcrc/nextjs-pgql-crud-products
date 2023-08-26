357318bdd4e4268e277794e0b02778caf4880e25
davidcrc <david_drcr@hotmail.com>
2023-08-26 10:42:03 -0500

    feat: list products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,8 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
-export function GET() {
-  return NextResponse.json({ message: "listing products" });
+export async function GET() {
+  try {
+    const products = await prisma.product.findMany();
+
+    return NextResponse.json(products);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(error.message, { status: 500 });
+    }
+  }
 }
 
 export async function POST(request: NextRequest) {
